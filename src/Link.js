3cd821aea11826cc902fd0204b6951b9d7937b6e
Dominik Rodler <dominik.rodler@gmail.com>
2017-07-17 13:51:57 +0200

    Use onMouseDown option
    This PR proposes to optionally use the mousedown event instead of the click event for the Link component.
    The default event handler is still the click event.
    
    Using onMouseDown instead of onClick is a common trick to speed up interactions, since the mousedown event fires ~200ms earlier than the click event.
    The only drawback is that you cannot cancel the click by moving the mouse away from the element.
    This is not a problem in most cases, but always and only triggering on mousedown is not desirable therefore.

--- src/Link.js
+++ src/Link.js
@@ -5,9 +5,9 @@ import requiredIf from 'react-required-if';
 import { push, replace, goBack, goForward } from 'redux-first-routing';
 
 const Link = (props) => {
-  const { to, action, onClick, children, dispatch, ...other } = props;
-
-  const handleClick = (event) => {
+  const { to, action, handler, onClick, children, dispatch, ...other } = props;
+  
+  const handleEvent = (event) => {
     // Ignore any click other than a left click
     if ((event.button && event.button !== 0)
       || event.metaKey
@@ -21,33 +21,43 @@ const Link = (props) => {
     // Prevent page reload
     event.preventDefault();
 
-    // Execute onClick callback, if it exists
-    if (onClick) {
-      onClick(event);
-    }
+    // Prevent event bubbling
+    event.stopPropagation();
 
-    // Dispatch the action specified in the props (default: push action)
-    if (action === 'replace') {
-      dispatch(replace(to));
-    } else if (action === 'back' || action === 'goBack') {
-      dispatch(goBack());
-    } else if (action === 'forward' || action === 'goForward') {
-      dispatch(goForward());
-    } else {
-      dispatch(push(to));
+    if (event.type === handler) {
+      // Execute onClick callback, if it exists
+      if (onClick) {
+          onClick(event);
+      }
+
+      // Dispatch the action specified in the props (default: push action)
+      if (action === 'replace') {
+        dispatch(replace(to));
+      } else if (action === 'back' || action === 'goBack') {
+        dispatch(goBack());
+      } else if (action === 'forward' || action === 'goForward') {
+        dispatch(goForward());
+      } else {
+        dispatch(push(to));
+      }
     }
   };
 
-  return (<a href={to} onClick={handleClick} {...other} >{children}</a>);
+  return (<a href={to} onMouseDown={handleEvent} onClick={handleEvent} {...other} >{children}</a>);
 };
 
 Link.propTypes = {
   to: requiredIf(PropTypes.string, (props) => !props.action),
   action: PropTypes.string,
+  handler: PropTypes.oneOf(['click', 'mousedown']),
   onClick: PropTypes.func,
   children: PropTypes.node,
   dispatch: PropTypes.func.isRequired,
 };
+
+Link.defaultProps = {
+  handler: 'onClick',
+}
 
 const LinkContainer = connect()(Link);
 
